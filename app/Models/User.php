5af4ae2ffa7d12a82577e62844e7d71c5a492384
Imad Asserne <imad.asserne@gmail.com>
2023-05-20 18:22:48 +0000

    add column & relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    public function etudiants()
+    {
+        return $this->hasMany(Etudiant::class);
+    }
 
     /**
      * The attributes that are mass assignable.
